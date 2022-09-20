1475614ec1797725059e3bf46c6c08ac78ef21a8
d-c0d1ng <92983208+d-c0d1ng@users.noreply.github.com>
2022-09-20 18:04:19 +0100

    Added interest finder logic

--- TBT_script.py
+++ TBT_script.py
@@ -23,7 +23,6 @@ def add_attraction(destination, attraction):
     return
 
 add_attraction("Los Angeles, USA", ["Venice Beach", ["beach"]])
-print(attractions)
 add_attraction("Paris, France", ["the Louvre", ["art", "museum"]])
 add_attraction("Paris, France", ["Arc de Triomphe", ["historical site", "monument"]])
 add_attraction("Shanghai, China", ["Yu Garden", ["garden", "historical site"]])
@@ -34,3 +33,19 @@ add_attraction("São Paulo, Brazil", ["São Paulo Zoo", ["zoo"]])
 add_attraction("São Paulo, Brazil", ["Pátio do Colégio", ["historical site"]])
 add_attraction("Cairo, Egypt", ["Pyramids of Giza", ["monument", "historical site"]])
 add_attraction("Cairo, Egypt", ["Egyptian Museum", ["museum"]])
+#print(attractions)
+
+def find_attractions(destination, interests):
+    destination_index = get_destination_index(destination)
+    attractions_in_city = attractions[destination_index]
+    attractions_with_interest = []
+    for attraction in attractions_in_city:
+        possible_attraction = attraction
+        attraction_tags = attraction[1]
+        for interest in interests:
+            if interest in attraction_tags:
+                attractions_with_interest.append(possible_attraction[0])
+    return attractions_with_interest
+
+la_arts = find_attractions("Los Angeles, USA", ['art'])
+print(la_arts)
